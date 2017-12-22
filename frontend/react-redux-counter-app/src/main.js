c24509c85c0f5b0448756960c38e2fce84234b3a
xstahol <staffan.holmgren@tre.se>
2017-12-22 11:22:00 +0100

    react-redux dev support

--- frontend/react-redux-counter-app/src/main.js
+++ frontend/react-redux-counter-app/src/main.js
@@ -7,7 +7,11 @@ import reducers from './reducers'
 import {loadState, saveState} from "./localStorage";
 
 const initialState = loadState();
-const store = createStore(reducers, initialState);
+const store = createStore(
+    reducers,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 
 store.subscribe(() => {
     saveState(store.getState());
