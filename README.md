c1a6de61910c6d77df22ad182b769effbcf1afdf
xstahol <staffan.holmgren@tre.se>
2017-12-22 09:32:39 +0100

    play framework 2.6.* with react redux skeleton code

---
+++ README.md
@@ -0,0 +1,9 @@
+Code lab with Play-scala react and redux
+=
+
+Compile different frontend apps under ./frontend with 
+- npm install
+- npm run generateFrontend
+
+run dev mode with
+- ./sbt run 
---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+
+  def reactreduxCounterApp() = Action { request =>
+    Ok(views.html.counterApp())
+  }
+}
