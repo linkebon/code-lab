2b1335f26f0aa9b7ca70b9f2505955c4ec81c048
xstahol <staffan.holmgren@tre.se>
2017-12-30 21:39:22 +0100

    styling for the todos

--- frontend/react-redux-todo-app/src/components/Todo.js
+++ frontend/react-redux-todo-app/src/components/Todo.js
@@ -15,22 +15,28 @@ class Todo extends Component {
         return (
             <div style={{whiteSpace: 'pre-line', border: '1px solid black', marginTop: '1%', padding: '1% 1% 1% 1%'}}>
                 <div className="pull-right">
-                    <a className={`text-muted`} data-toggle="collapse" href={'#' + this.props.id}
-                       aria-expanded="false"
-                       aria-controls={'#' + this.props.id}
-                       onClick={(e) => {
-                           e.preventDefault();
-                           toggleTodo(this.props.id, !this.props.collapsed);
-                       }}>
-                        {new Date(this.props.id).toLocaleString()}</a>
-                    <button className="btn-danger" style={{marginLeft: '10px'}} onClick={(e) => {
+                    <a className={`text-muted`}>
+                        {new Date(this.props.id).toLocaleString()}
+                    </a>
+
+                    <button className="btn-default" style={{marginLeft: '10px'}} data-toggle="collapse"
+                            href={'#' + this.props.id}
+                            aria-expanded="false"
+                            aria-controls={'#' + this.props.id}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                toggleTodo(this.props.id, !this.props.collapsed);
+                            }}>-
+                    </button>
+                    <button className="btn-danger" onClick={(e) => {
                         e.preventDefault();
                         removeTodo(this.props.id);
                     }}>x
                     </button>
                 </div>
                 <br/>
-                <div id={this.props.id} className={`collapse ${collapsed ? '' : 'in'}`} aria-expanded={`${collapsed ? 'false' : 'true'}`}>
+                <div id={this.props.id} className={`collapse ${collapsed ? '' : 'in'}`}
+                     aria-expanded={`${collapsed ? 'false' : 'true'}`}>
                     <p>{this.props.text}</p>
                 </div>
             </div>
